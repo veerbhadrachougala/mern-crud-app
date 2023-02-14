eac435811d3d588b5d79569e7b241b5165a88406
veerbhadrachougala <chougalav25@gmail.com>
2023-02-14 21:01:30 +0530

    made some changes

--- server/database/db.js
+++ server/database/db.js
@@ -1,11 +1,11 @@
 import mongoose from "mongoose";
 
 const connection = async (username, password) => {
-  const URL =
+  const DB =
     `mongodb+srv://${username}:${password}@cluster0.wgfkgxm.mongodb.net/veerdb?retryWrites=true&w=majority`;
 
   try {
-    await mongoose.connect(URL, {
+    await mongoose.connect(DB, {
       useUnifiedTopology: true,
       useNewUrlParser: true,
     });
